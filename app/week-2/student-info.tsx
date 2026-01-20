6d80fb0728b495a94828926f1f73bb9670540088
aryansaini-71 <aryan9817692114@gmail.com>
2026-01-20 15:42:11 -0700

    Fixing build issues

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Name : Aryan</p>
+        <a href="https://github.com/aryansaini-71">GitHub Link: https://github.com/aryansaini-71</a>
+    </div>
+  );
+}
